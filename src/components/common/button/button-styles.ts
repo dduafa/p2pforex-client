e625d2bc1c092b9d2e58be93de22771e20640304
kitchenblade <reign.derek@yahoo.com>
2022-03-10 19:07:10 +0000

    feat(button): add styled button

--- src/components/common/button/button-styles.ts
+++ src/components/common/button/button-styles.ts
@@ -1,36 +1,5 @@
 import styled from 'styled-components';
 
-// export const StyledButton = styled.button`
-//   background-color: #2f8bfd;
-//   color: #ffffff;
-//   min-width: 16px;
-//   border: 0;
-//   border-radius: 4px;
-//   padding: 8px 16px;
-//   outline: none;
-//   font-size: 0.874rem;
-//   line-height: 1.5;
-//   letter-spacing: 0.02857rem;
-//   cursor: pointer;
-//   transition: all 0.2s;
-//   &:hover {
-//     background-color: #0072ff;
-//   }
-// `;
-
-interface Props {
-  $color?: string;
-  $padding?: string;
-  $width?: string;
-  $bgColor?: string;
-  $margin?: string;
-  $position?: string;
-  $align?: string;
-  $top?: string;
-  $boxShadow?: string;
-  $border?: string;
-}
-
 export const StyledButton = styled.button`
   height: 40px;
   width: 100%;
