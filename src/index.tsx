a8d809c49a54b95385c73a9b0f60bd68d39979e0
kitchenblade <reign.derek@yahoo.com>
2022-03-10 18:02:05 +0000

    feat(loader): add loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <GlobalStyle /> */}
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  * {
+  *,
+  *::before,
+  *::after {
     margin: 0;
     padding: 0;
     outline: 0;
@@ -14,9 +16,23 @@ export default createGlobalStyle`
   }
 
   html {
+    /* font-size: 62.5%; */
       -webkit-background-size: cover;
       -moz-background-size: cover;
       -o-background-size: cover;
       background-size: cover;
+      display: 'flex';
+      align-items: 'center';
+      justify-content:'center';
   }
 `;
+
+/*
+$s-size: 1.2rem;
+$m-size: 1.6rem;
+$l-size: 3.2rem;
+$xl-size: 4.8rem;
+
+html {
+  font-size: 62.5%; /* 62.5% of 16px = 10px 
+*/
