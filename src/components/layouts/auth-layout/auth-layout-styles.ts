7772b2d1c5e95b23c88ec5acde3b90b6b675e7c4
kitchenblade <reign.derek@yahoo.com>
2022-03-12 20:02:36 +0000

    refactor: remove comments in auth layout

--- src/components/layouts/auth-layout/auth-layout-styles.ts
+++ src/components/layouts/auth-layout/auth-layout-styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-// import AuthImage from '@assets/images/auth-background.jpg';
 
 export const MainWrapper = styled.main`
   display: flex;
@@ -9,11 +8,4 @@ export const MainWrapper = styled.main`
   height: 100vh;
   width: 100%;
   background: #EAE9E7;
-  /* background-image: url(https://images.unsplash.com/photo-1565372781813-6e4d16bb4a3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=80); */
-  /* background-repeat: no-repeat; */
-  /* background-size: 50% 100vh; */
-
-  /* @media (max-width: 550px) {
-    background-size: cover;
-  } */
 `;
