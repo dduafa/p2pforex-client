0b8e705213144d3badfede6a935e85a33f6631eb
kitchenblade <reign.derek@yahoo.com>
2022-03-28 21:38:38 +0000

    refactor auth test

--- src/features/auth/auth-reducer.spec.ts
+++ src/features/auth/auth-reducer.spec.ts
@@ -17,13 +17,16 @@ const updateState = ({
 });
 describe('auth-reducer', () => {
   it('should return the initial state, given NO `initialState` and NO `action`', () => {
-    const actualState = authReducer(undefined, {} as AnyAction);
+    const initialState = undefined;
+    const action = { type: '' } as AnyAction;
+    const actualState = authReducer(initialState, action);
     const expectedState = updateState({});
     expect(actualState).toEqual(expectedState);
   });
 
   it('should set `isAuthenticated` to true, given NO `initialState` and calling `setAuthenticated(true)` action', () => {
-    const actualState = authReducer(undefined, setAuthenticated(true));
+    const initialState = undefined;
+    const actualState = authReducer(initialState, setAuthenticated(true));
     const expectedState = updateState({ isAuthenticated: true });
     expect(actualState).toEqual(expectedState);
   });
